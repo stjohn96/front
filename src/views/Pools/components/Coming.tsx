5590b56e0c5e2ea0344c39f29de888f057a99b3f
bombec49 <bombec49@koaladefi.finance>
2021-04-15 17:32:39 +0200

    update coming icon

--- src/views/Pools/components/Coming.tsx
+++ src/views/Pools/components/Coming.tsx
@@ -43,7 +43,7 @@ const Coming: React.FC = () => {
             👀
           </span>
         </CardTitle>
-        <Image src="/images/bunny-question.svg" width={64} height={64} alt="Your project here" />
+        <Image src="/images/yourcoinkoaladefi.png" width={64} height={64} alt="Your project here" />
         <Balance>???</Balance>
         <Label>{TranslateString(416, 'Create a pool for your token')}</Label>
         <Button variant="secondary" as="a" href="https://t.me/koaladefichat" external width="100%" mb="16px">
